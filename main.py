06eaf259214ca42af4888ffd2557f4d546394794
nitobenito <52706184+nitobenito@users.noreply.github.com>
2020-04-30 13:21:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = Flask('app')
 
 @app.route('/', methods=["GET", "POST"])
 def index_lapa():
-    return varda_diena("Aiva")
+    return vd_kad('Aiva')
     #return render_template('index.html')
 
 
